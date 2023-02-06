2202b945309b0a23245ebb88affcf6754b29274b
Nils Leusmann <leusmann@uni-bremen.de>
2023-02-06 09:51:35 +0100

    Init Commit

---
+++ Source/FoodNinja/FoodNinjaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FoodNinjaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOODNINJA_API AFoodNinjaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
